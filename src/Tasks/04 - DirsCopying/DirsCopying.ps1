dd541a12999848a5c8cc59c55670dc9035b1eb2b
Aliwoto <woto@kaizoku.cyou>
2022-12-18 01:10:24 +0330

    Add source code for Task04's script.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

---
+++ src/Tasks/04 - DirsCopying/DirsCopying.ps1
@@ -0,0 +1,15 @@
+# PowerShellSimpleTasks Project
+# Copyright (C) 2022 ALiwoto
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE', which is part of the source code.
+
+function Invoke-TaskDirsCopying {
+    param (
+        [string[]]$SourceDirsName,
+        [string[]]$Destination
+    )
+    
+    foreach ($currentDestination in $Destination) {
+        Copy-Item -Path $SourceFilesName -Destination $currentDestination -Force
+    }
+}
