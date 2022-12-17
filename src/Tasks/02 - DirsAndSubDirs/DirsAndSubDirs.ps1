66b13a9789d9e0f92b632f02b8fb941c5c6a386b
Aliwoto <woto@kaizoku.cyou>
2022-12-18 00:43:32 +0330

    Add second task's script souce code.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

---
+++ src/Tasks/02 - DirsAndSubDirs/DirsAndSubDirs.ps1
@@ -0,0 +1,15 @@
+# PowerShellSimpleTasks Project
+# Copyright (C) 2022 ALiwoto
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE', which is part of the source code.
+
+function Invoke-TaskDirsAndSubDirs {
+    param (
+        [string[]]$ParentDirName,
+        [string[]]$ChildrenDirName
+    )
+
+    foreach ($currentParent in $ChildrenDirName) {
+        New-Item -Path $ParentDirName -Name $currentParent -ItemType "Directory"
+    }
+}
