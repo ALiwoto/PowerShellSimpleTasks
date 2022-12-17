6c48f6706f4e22daa58d4846ca2b1de9640614fd
Aliwoto <woto@kaizoku.cyou>
2022-12-18 00:42:44 +0330

    Add and implement Task02: DirsAndSubDirs.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- RunTasks.ps1
+++ RunTasks.ps1
@@ -27,6 +27,11 @@ foreach ($currentDir in Get-ChildItem -Path ".\src\Tasks\" -Directory) {
     Write-Debug $currentDir
 }
 
+# Reads and returns the options provided by the user (from console)
+# as an array of strings separated by comma and/or whitespace.
+function Read-UserOptions {
+    return ((Read-Host) -split "[, ]" | Where-Object {$_.Trim().Length -ne 0 } )
+}
 
 "Welcome to the PowerShellSimpleTask script!`n" +
 "This script is designed to do some simple tasks"+
@@ -34,17 +39,17 @@ foreach ($currentDir in Get-ChildItem -Path ".\src\Tasks\" -Directory) {
 
 function Show-MainMenu {
     while ($true) {
-        "Please select which task you would like to run?" | Write-Host
+        "`nPlease select which task you would like to run?" | Write-Host
         "1- ServiceList`n" +
         "2- DirsAndSubDirs`n" | Write-Host
     
         $userInput = Read-Host
         switch ($userInput) {
             "1" {
-                "This task will output a list of services, matched with the " +
-                "word(s) you are providing (provide an empty string to list all " +
+                "This task will output a list of services, matching with the " +
+                "word(s) you have provided (provide an empty string to list all " +
                 "or separate them using comma `",`":" | Write-Host
-                $serviceName = (Read-Host) -split ","
+                $serviceName = Read-UserOptions
                 
                 "Would you also like to apply a limit to list of services?" | Write-Host
                 $serviceLimit = Read-Host
@@ -52,7 +57,28 @@ function Show-MainMenu {
                 Invoke-TaskServiceList -Name $serviceName -ServiceLimit $serviceLimit
             }
             "2" {
-                "Here" | Write-Host
+                "This task will create some sub-directories under the specified " +
+                "directories.`n" +
+                "Give me the name of parents directories " +
+                "(separated by comma): " | Write-Host
+                $allParents = Read-UserOptions
+                if (($null -eq $allParents) -or ($allParents.Length -eq 0)) {
+                    # make sure parent directories are not null or empty.
+                    break
+                }
+
+                "Give me the name of children directories " +
+                "(separated by comma): " | Write-Host
+                $allChildren = Read-UserOptions
+                if (($null -eq $allChildren) -or ($allChildren.Length -eq 0)) {
+                    # make sure children sub-directories are not null or empty.
+                    break
+                }
+
+                Invoke-TaskDirsAndSubDirs -ParentDirName $allParents -ChildrenDirName $allChildren
+            }
+            "3" {
+                
             }
             Default {
                 "Thanks for taking time and testing out this script!" | Write-Host
